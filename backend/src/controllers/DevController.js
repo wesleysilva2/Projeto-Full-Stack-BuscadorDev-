02e4b0b2b88121d8c2b21e4d686717eb9b0226eb
wesleysilva2 <ede8021@gmail.com>
2024-04-10 19:10:50 -0300

    Adicionando metodo de atualização de cadastro e exclusão de cadastro no Devcontroller

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -43,5 +43,60 @@ module.exports = {
         }
 
         return response.json(dev); 
+    },
+
+    async update(request, response){
+       const {github_username} = request.query; // Captura o github user do desenvolvedor que sera usado como um ID pra identificar
+       const {name, bio, techs, latitude, longitude, avatar_url} = request.body; // Dados a serem atualizados
+       
+       // Verifica se o desenvolvedor existe
+       let dev = await Dev.findOne({ github_username })
+
+       if(!dev){
+          return response.status(404).json({error: 'Desenvolvedor não encontrado na base de dados'});
+       }
+
+       // Atualiza os dados do desenvolvedor
+       if(techs){
+        dev.techs = parseStringAsArray(techs);
+       }
+
+       if(latitude && longitude){
+        dev.location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+        };
+       }
+
+       if(name){
+        dev.name = name;
+       }
+
+       if(bio){
+        dev.bio = bio;
+       }
+
+       if(avatar_url){
+        dev.avatar_url = avatar_url;
+       }
+       
+       // Salva as alterações no banco de dados
+       await dev.save();
+       
+       // Retorna o desenvolvedor atualizado
+       return response.json(dev);
+    },
+
+    async destroy(request, response){
+        const {github_username} = request.query;
+
+        let dev = await Dev.findOne({ github_username });
+
+        if(!dev){
+            return response.status(404).json({error: 'Desenvolvedor não encontrado na base de dados'});
+        }
+
+        await dev.deleteOne();
+
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,17 @@
-const { Router } = require('express') // Pegando apenas o modulo Router do Express, so vai precisar disso aqui
+const { Router } = require('express'); // Pegando apenas o modulo Router do Express, so vai precisar disso aqui
 
-const DevController = require('./controllers/DevController')
-const SearcheController = require('./controllers/SearcheController')
+const DevController = require('./controllers/DevController');
+const SearcheController = require('./controllers/SearcheController');
 
-const routes = Router()
+const routes = Router();
 
 // Sempre se tem a requisição e a resposta
 // Quando eu acesso uma Rota eu estou fazendo uma requisição e ela tem uma resposta
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store); // store é o nome da função em devController
+routes.put('/devs', DevController.update);
+routes.delete('/devs', DevController.destroy);
 
-routes.get('/search', SearcheController.index)
+routes.get('/search', SearcheController.index);
 
 module.exports = routes; // Exportando as rotas para que a aplicação conheça as rotas definidas aqui
